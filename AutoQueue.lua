d8694cb1146dcf316c15962aaa7f0fb80e192fd7
lnx00 <34384416+lnx00@users.noreply.github.com>
2022-03-26 00:31:22 +0100

    Added AutoQueue

---
+++ AutoQueue.lua
@@ -0,0 +1,23 @@
+AutoQueue = true
+local lastTime = 0
+
+function Draw_AutoQueue()
+    if not AutoQueue or gamecoordinator.HasLiveMatch() or gamecoordinator.IsConnectedToMatchServer() then
+        return
+    end
+
+    if globals.RealTime() - lastTime < 5 then
+        return
+    end
+
+    lastTime = globals.RealTime()
+    if #party.GetQueuedMatchGroups() == 0 and not party.IsInStandbyQueue() then
+        party.QueueUp(7)
+    end
+end
+
+-- Can't use CreateMove, because it's not called in main menu
+callbacks.Unregister("Draw", "Draw_AutoQueue")
+callbacks.Register("Draw", "Draw_AutoQueue", Draw_AutoQueue)
+
+engine.Notification("AutoQueue info", "You have just executed the AutoQueue script.\nIf you want to stop it, simply type this into the console:\nlua AutoQueue = false\n\nYou can re-enable AutoQueue again by running the script again or by typing:\nlua AutoQueue = true")
