9634d378bc2880a7cf0e9e076deddc86f1c3024b
lnx00 <34384416+lnx00@users.noreply.github.com>
2022-04-09 16:40:59 +0200

    Update Menu-Example.lua

--- Menu-Example.lua
+++ Menu-Example.lua
@@ -1,15 +1,17 @@
 local MenuLib = require("Menu")
 
-function OnButtonPress() 
-    print("Button pressed!")
-end
-
 -- Create our first menu
 local menu = MenuLib.Create("Example Menu", MenuFlags.AutoSize)
 menu:AddComponent(MenuLib.Label("This is our first menu."))
 local checkbox = menu:AddComponent(MenuLib.Checkbox("Enable Feature", true))
+
+-- Button with callback
+function OnButtonPress() 
+    print("Button pressed!")
+end
 menu:AddComponent(MenuLib.Button("Press Me!", OnButtonPress))
 
+-- Combobox
 local itemCombo = {
     "Label",
     "Checkbox"
@@ -25,6 +27,7 @@ function AddElement()
 end
 menu:AddComponent(MenuLib.Button("Add Element!", AddElement))
 
+-- Multi Combobox
 local multiCombo = {
     { "Head", true },
     { "Body", false },
