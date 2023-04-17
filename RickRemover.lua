4d35547665a2a255cf783156f1e29bb2a39920e6
lnx00 <linch255@gmail.com>
2023-04-17 21:08:14 +0200

    Removed useless line

--- RickRemover.lua
+++ RickRemover.lua
@@ -2,7 +2,6 @@ materials.Enumerate(function (material)
     local name = material:GetName()
     if name:find("models/soldier_statue") then
         print("Hiding: " .. name)
-        material:AlphaModulate(0.0)
         material:SetMaterialVarFlag(MATERIAL_VAR_NO_DRAW, true)
     end
 end)
