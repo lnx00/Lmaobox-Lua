e7fc904bcd96abb242dcc4607cc15f8635baf97c
lnx00 <34384416+lnx00@users.noreply.github.com>
2023-06-03 01:40:20 +0200

    Fixed and rewrote cheater detection

--- WIP/Detections.lua
+++ WIP/Detections.lua
@@ -3,94 +3,74 @@
     Author: LNX (github.com/lnx00)
 ]]
 
+---@alias PlayerData { Angle : EulerAngles[], Position : Vector3[], SimTime : number[] }
+
+---@type boolean, lnxLib
+local libLoaded, lnxLib = pcall(require, "lnxLib")
+assert(libLoaded, "lnxLib not found, please install it!")
+assert(lnxLib.GetVersion() >= 0.981, "lnxLib version is too old, please update it!")
+
+local WPlayer = lnxLib.TF2.WPlayer
+
 local options = {
     StrikeLimit = 3,
-    MaxAngleDelta = 50
+    MaxAngleDelta = 50,
+    AutoMark = true
 }
 
-local playerStrikes = {}
-local oldSimTimes = {}
-local oldEyeAngles = {}
+local prevData = {} ---@type PlayerData
+local playerStrikes = {} ---@type table<number, number>
 
-local function StrikePlayer(index, reason)
-    if playerStrikes[index] == nil then
-        playerStrikes[index] = 1
-    elseif playerStrikes[index] >= 0 then
-        playerStrikes[index] = playerStrikes[index] + 1
-        client.ChatPrintf("\x04[CD] \x02Player\x05 " .. index .. " \x02striked for:\x05 " .. reason)
+local function StrikePlayer(idx, reason)
+    if not playerStrikes[idx] then
+        playerStrikes[idx] = 0
+    end
+
+    playerStrikes[idx] = playerStrikes[idx] + 1
+
+    if playerStrikes[idx] < options.StrikeLimit then
+        client.ChatPrintf(string.format("\x04[CD] \x01Player \x05%d \x01has been striked for \x05%s", idx, reason))
+    elseif playerStrikes[idx] == options.StrikeLimit then
+        client.ChatPrintf(string.format("\x04[CD] \x01Player \x05%d \x01is cheating!", idx))
     end
 end
 
--- Check if a player is choking packets (Fakelag, Doubletap)
-local function CheckChoke(player)
-    local simTime = player:GetPropFloat("m_flSimulationTime")
-    if not simTime then return end
-
-    if not oldSimTimes[player:GetIndex()] then
-        oldSimTimes[player:GetIndex()] = simTime
-        return
-    end
-
-    local delta = simTime - oldSimTimes[player:GetIndex()]
-    if delta > 1 then
-        StrikePlayer(player:GetIndex(), "Packet was choked")
-    end
-
-    oldSimTimes[player:GetIndex()] = simTime
-end
-
--- Check for invalid pitch (Fake pitch)
+---@param player WPlayer
 local function CheckPitch(player)
-    local eyeAnglesX = player:GetPropFloat("tfnonlocaldata", "m_angEyeAngles[0]")
-    if not eyeAnglesX then return end
+    local angles = player:GetEyeAngles()
 
-    if (eyeAnglesX ~= 0) and (eyeAnglesX >= 90 and eyeAnglesX <= -90) then
-        StrikePlayer(player:GetIndex(), "Invalid Pitch")
+    if angles.pitch >= 90 or angles.pitch <= -90 then
+        StrikePlayer(player:GetIndex(), "Invalid pitch")
     end
 end
 
--- Check angle delta (Silent Aim)
-local function CheckAngleDelta(player)
-    local eyeAngles = player:GetPropVector("tfnonlocaldata", "m_angEyeAngles")
-    if not eyeAngles then return end
+---@param userCmd UserCmd
+local function OnCreateMove(userCmd)
+    local me = WPlayer.GetLocal()
+    if not me then return end
 
-    if not oldEyeAngles[player:GetIndex()] then
-        oldEyeAngles[player:GetIndex()] = eyeAngles
-        return
-    end
-
-    local delta = eyeAngles - oldEyeAngles[player:GetIndex()]
-    if delta:Length() > options.MaxAngleDelta then
-        StrikePlayer(player:GetIndex(), "Invalid Angle Delta")
-    end
-
-    oldEyeAngles[player:GetIndex()] = eyeAngles
-end
-
-local function OnCreateMove(pCmd)
+    -- Get current data
+    local currentData = {
+        Angle = {},
+        Position = {},
+        SimTime = {}
+    }
     local players = entities.FindByClass("CTFPlayer")
-    for k, vPlayer in pairs(players) do
-        if vPlayer:IsValid() == false or vPlayer:IsAlive() == false then
-            oldEyeAngles[vPlayer:GetIndex()] = nil
-            goto continue
-        end
+    for idx, entity in pairs(players) do
+        if idx ~= me:GetIndex() then goto continue end
+        if entity:IsDormant() or not entity:IsAlive() then goto continue end
+        local player = WPlayer.FromEntity(entity)
 
-        CheckChoke(vPlayer)
-        CheckPitch(vPlayer)
-        CheckAngleDelta(vPlayer)
+        currentData.Angle[idx] = player:GetEyeAngles()
+        currentData.Position[idx] = player:GetAbsOrigin()
+        currentData.SimTime[idx] = player:GetSimulationTime()
+
+        CheckPitch(player)
 
         ::continue::
     end
 
-    for kIndex, vStrikes in pairs(playerStrikes) do
-        if vStrikes == options.StrikeLimit then
-            client.ChatPrintf("\x04[CD] \x02Cheater detected:\x05 " .. entities.GetByIndex(kIndex):GetName())
-            playerStrikes[kIndex] = -1
-        end
-    end
+    prevData = currentData
 end
 
-callbacks.Unregister("CreateMove", "CD_CreateMove")
-callbacks.Register("CreateMove", "CD_CreateMove", OnCreateMove)
-
-client.Command('play "ui/buttonclick"', true)
+callbacks.Register("CreateMove", OnCreateMove)
