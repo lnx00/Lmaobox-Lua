55fbde1cf7bc9cc09936c22a93ec85deeae4de95
lnx00 <34384416+lnx00@users.noreply.github.com>
2023-06-13 22:50:59 +0200

    Added simple strafe prediction to aimbot

--- PoC/Aimbot.lua
+++ PoC/Aimbot.lua
@@ -32,15 +32,53 @@ local options = {
     Silent = true,
     AimPos = {
         Hitscan = Hitbox.Head,
-        Projectile = Hitbox.Body
+        Projectile = Hitbox.Head
     },
     AimFov = 40,
     PredTicks = 60,
-    Debug = true
+    StrafePrediction = true,
+    StrafeSamples = 1,
+    DebugInfo = true
 }
 
 local latency = 0
 local lerp = 0
+local lastAngles = {} ---@type EulerAngles[]
+local strafeAngles = {} ---@type number[]
+
+---@param me WPlayer
+local function CalcStrafe(me)
+    local players = entities.FindByClass("CTFPlayer")
+    for idx, entity in ipairs(players) do
+        if entity:IsDormant() or not entity:IsAlive() then
+            lastAngles[idx] = nil
+            strafeAngles[idx] = nil
+            goto continue
+        end
+
+        -- Ignore teammates (for now)
+        if entity:GetTeamNumber() == me:GetTeamNumber() then
+            goto continue
+        end
+
+        local v = entity:EstimateAbsVelocity()
+        local angle = v:Angles()
+
+        -- Play doesn't have a last angle
+        if lastAngles[idx] == nil then
+            lastAngles[idx] = angle
+            goto continue
+        end
+
+        -- Calculate the delta angle
+        if angle.y ~= lastAngles[idx].y then
+            strafeAngles[idx] = angle.y - lastAngles[idx].y
+        end
+        lastAngles[idx] = angle
+
+        ::continue::
+    end
+end
 
 -- Finds the best position for hitscan weapons
 ---@param me WPlayer
@@ -87,7 +125,8 @@ local function CheckProjectileTarget(me, weapon, player)
     end
 
     -- Predict the player
-    local predData = Prediction.Player(player, options.PredTicks)
+    local strafeAngle = strafeAngles[player:GetIndex()]
+    local predData = Prediction.Player(player, options.PredTicks, strafeAngle)
     if not predData then return nil end
 
     -- Find a valid prediction
@@ -195,10 +234,13 @@ local function OnCreateMove(userCmd)
     if not input.IsButtonDown(options.AimKey) then return end
 
     local me = WPlayer.GetLocal()
-    if not me then return end
+    if not me or not me:IsAlive() then return end
 
     local weapon = me:GetActiveWeapon()
     if not weapon then return end
+
+    -- Calculate strafe angles
+    CalcStrafe(me)
 
     -- Check if we can shoot
     local flCurTime = globals.CurTime()
@@ -228,11 +270,23 @@ local function OnCreateMove(userCmd)
 
     -- Auto Shoot
     if options.AutoShoot then
-        userCmd.buttons = userCmd.buttons | IN_ATTACK
+        if weapon:GetDefIndex() == 56 then
+            -- Huntsman
+            if weapon:GetChargeBeginTime() > 0 then
+                userCmd.buttons = userCmd.buttons & ~IN_ATTACK
+            else
+                userCmd.buttons = userCmd.buttons | IN_ATTACK
+            end
+        else
+            -- Normal weapon
+            userCmd.buttons = userCmd.buttons | IN_ATTACK
+        end
     end
 end
 
 local function OnDraw()
+    if not options.DebugInfo then return end
+
     draw.SetFont(Fonts.Verdana)
     draw.Color(255, 255, 255, 255)
 
@@ -241,7 +295,15 @@ local function OnDraw()
     draw.Text(20, 160, string.format("Lerp: %.2f", lerp))
 
     local me = WPlayer.GetLocal()
-    if not me then return end
+    if not me or not me:IsAlive() then return end
+
+    local weapon = me:GetActiveWeapon()
+    if not weapon then return end
+
+    -- Draw current weapon
+    draw.Text(20, 180, string.format("Weapon: %s", weapon:GetName()))
+    draw.Text(20, 200, string.format("Weapon ID: %d", weapon:GetWeaponID()))
+    draw.Text(20, 220, string.format("Weapon DefIndex: %d", weapon:GetDefIndex()))
 end
 
 callbacks.Unregister("CreateMove", "LNX.Aimbot.CreateMove")
